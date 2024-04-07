381aa16c24f865064aebd3d7b874e980191d91ff
ahmed khalil amrani <amraniahmedkhalil@gmail.com>
2024-04-07 02:01:56 +0100

    feat: heroCarousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
+import HeroCarousel from "@/components/heroCarousel/heroCarousel";
+
 export default function Home() {
   return (
-    <main className="z-0">
-      <div className="flex flex-col items-center  w-full h-screen">
-        <h1 className="text-6xl font-bold">Hello, World!</h1>
-      </div>
-
-      <div className="flex flex-col items-center w-full h-screen">
-        <h1 className="text-6xl font-bold">Hello, World!</h1>
-      </div>
-    </main>
+    <>
+      <HeroCarousel />
+    </>
   );
 }
---
+++ components/heroCarousel/heroCarousel.tsx
@@ -0,0 +1,100 @@
+"use client";
+import Image from "next/image";
+import React, { MouseEventHandler, use, useState } from "react";
+import pic from "../../assets/pic.jpg";
+import pic2 from "../../assets/pic2.jpg";
+import pic3 from "../../assets/pic3.jpg";
+import pic4 from "../../assets/pic4.jpg";
+
+import { motion, useMotionValue } from "framer-motion";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+type Props = {};
+const images = [pic.src, pic2.src, pic3.src, pic4.src];
+export default function HeroCarousel({}: Props) {
+  const dragAmount = useMotionValue(0);
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const handleDragEnd = () => {
+    if (dragAmount.get() < -50 && currentSlide < images.length - 1) {
+      setCurrentSlide((prev) => prev + 1);
+    } else if (dragAmount.get() > 50 && currentSlide > 0) {
+      setCurrentSlide((prev) => {
+        return prev == 0 ? 0 : prev - 1;
+      });
+    }
+
+    // dragAmount.set(0);
+  };
+  const handleOnLeftClick = () => {
+    if (currentSlide > 0) {
+      setCurrentSlide((prev) => prev - 1);
+    }
+  };
+  const handleOnRightClick = () => {
+    if (currentSlide < images.length - 1) {
+      setCurrentSlide((prev) => prev + 1);
+    }
+  };
+
+  return (
+    <>
+      <section className="relative mx-6 mt-6 mb-4 overflow-hidden lg:mx-24 xl:mx-44 rounded-2xl">
+        <motion.div
+          drag="x"
+          className="flex w-full cursor-grab active:cursor-grabbing"
+          animate={{
+            translateX: `-${currentSlide * 100}%`,
+          }}
+          style={{
+            x: dragAmount,
+          }}
+          transition={{
+            type: "spring",
+            mass: 3,
+            stiffness: 250,
+            damping: 35,
+            duration: 0.5,
+          }}
+          // onDragStart={}
+          onDragEnd={handleDragEnd}
+          dragConstraints={{ left: 0, right: 0 }}
+        >
+          {images.map((imgSrc, index) => (
+            <div
+              key={index}
+              style={{
+                backgroundImage: `url(${imgSrc})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+              }}
+              className="w-full px-20 rounded-2xl h-60 lg:h-72 xl:h-96 shrink-0"
+            />
+          ))}
+        </motion.div>
+        <div
+          className="absolute top-0 bottom-0 left-0 flex items-center justify-center lg:w-24 h-full cursor-pointer bg-gradient-to-r from-[#0155B8]/30 via-[#0155B8]/20 to-transparent backdrop-blur-sm"
+          onClick={handleOnLeftClick}
+        >
+          {/* left arrow */}
+          <ChevronLeft color="#FFFF" className="w-12 h-12 lg:w-16 lg:h-16 " />
+        </div>
+        <div
+          className="absolute top-0 bottom-0 right-0 flex items-center justify-center lg:w-24 h-full cursor-pointer bg-gradient-to-l from-[#0155B8]/30 via-[#0155B8]/20 to-transparent backdrop-blur-sm"
+          onClick={handleOnRightClick}
+        >
+          {/* right arrow */}
+          <ChevronRight color="#FFFF" className="w-12 h-12 lg:w-16 lg:h-16 " />
+        </div>
+      </section>
+      <div className="flex justify-center">
+        {images.map((_, index) => (
+          <div
+            key={index}
+            className={`w-3 h-3 mx-1 rounded-full ${
+              currentSlide == index ? "bg-[#0155B8]" : "bg-[#0155B8]/30"
+            }`}
+          />
+        ))}
+      </div>
+    </>
+  );
+}
