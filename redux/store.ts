2f233d50cc119d192df174d475bbb4df2fe59b35
ahmed khalil amrani <amraniahmedkhalil@gmail.com>
2024-06-18 14:40:04 +0100

    feat: redux configuration

--- redux/store.ts
+++ redux/store.ts
@@ -1,7 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import {userSlice} from "@/redux/user/userSlice";
 
-const store = configureStore({
+export const store = configureStore({
     reducer:{
-
+        user: userSlice.reducer
     }
 })
+
+export type RootState = ReturnType<typeof store.getState>
+
+export type AppDispatch = typeof store.dispatch
