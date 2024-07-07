132ace2ce4a84a76123e0cdfda282252f7684d27
ahmed khalil amrani <amraniahmedkhalil@gmail.com>
2024-07-07 13:38:49 +0100

    feat: shadcn components

--- lib/zod.ts
+++ lib/zod.ts
@@ -16,4 +16,20 @@ export const zodSchemas = {
         userFirstName: z.string(),
         userLastName: z.string()
     }),
-}
+    addUserSchema: z.object({
+        userEmail: z.string().email(),
+        userPassword: z.string().min(8, "Password must be at least 8 characters long"),
+        userFirstName: z.string().min(1),
+        userLastName: z.string().min(1),
+        userRoleId: z.string().min(1)
+    }),
+    editUserSchema: z.object({
+        userId: z.string().optional(),
+        userEmail: z.string().email(),
+        userFirstName: z.string().min(1),
+        userLastName: z.string().min(1),
+        userRole: z.object({
+            userRoleId: z.number().min(1),
+        }),
+    }),
+};
