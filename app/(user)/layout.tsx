380a822c6d8cfe77156425d81fe8f7565688c951
ahmed khalil amrani <amraniahmedkhalil@gmail.com>
2024-07-22 20:26:50 +0100

    feat: initial footer

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -3,7 +3,7 @@ import {Inter} from "next/font/google";
 import "./globals.css";
 import {cn} from "@/lib/utils";
 import NavBar from "@/components/navBar/navBar";
-import {ReactNode} from "react";
+import React, {ReactNode} from "react";
 import AuthProvider from "@/redux/authProvider";
 import {Toaster} from '@/components/ui/toaster'
 
@@ -21,10 +21,20 @@ export default function RootLayout({
     return (
         <html lang="en">
         <AuthProvider>
-            <body className={cn(" overflow-x-hidden bg-slate-100/30", inter.className)}>
+            <body className={cn(" overflow-x-hidden bg-slate-100/30 relative", inter.className)}>
             <NavBar/>
             {children}
             <Toaster/>
+            <footer className={" absolute -bottom-28 w-full h-fit"}>
+                <div className=" absolute h-[2px] bg-primaryColor w-full"></div>
+                <div
+                    className="px-10 py-3 flex flex-col justify-center items-center gap-3 h-20 text-gray-800 bg-white ">
+                    <p className={" font-medium text-lg"}>© {new Date().getFullYear()} XPMarket</p>
+                    <p className={"text-lg"}>Made with <span className={"text-red-600"}>&#10084;</span> by <span
+                        className={"font-bold"}>Ahmed Khalil</span>
+                    </p>
+                </div>
+            </footer>
             </body>
         </AuthProvider>
         </html>
