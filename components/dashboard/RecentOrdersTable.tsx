dc66a59579885e28660d211994bad45b37f44a68
ahmed khalil amrani <amraniahmedkhalil@gmail.com>
2024-07-24 21:48:18 +0100

    fix: overflow when text long

--- components/dashboard/RecentOrdersTable.tsx
+++ components/dashboard/RecentOrdersTable.tsx
@@ -12,15 +12,15 @@ function RecentOrdersTable({token}: Props) {
     const {data, isLoading, isError} = useRecentOrdersQuery(token);
 
     return (<div
-        className={"flex w-full md:w-1/5 flex-grow flex-col gap-6 py-4 px-6 rounded-lg border-2 border-gray-300 bg-white drop-shadow-md"}>
+        className={"flex w-full lg:w-1/4  flex-col gap-6 py-4 px-6 rounded-lg border-2 border-gray-300 bg-white drop-shadow-md"}>
         <h2 className={" font-medium text-lg"}>Recent orders</h2>
         {data?.map((order) =>
-            (<div key={order.orderId} className={"flex items-center justify-between"}>
-                <div className={" flex items-center gap-5"}>
-                    <CircleUserRound size={32}/>
-                    <div className={""}>
+            (<div key={order.orderId} className={"flex items-center justify-between max-w-full gap-2"}>
+                <div className={" flex items-center gap-3"}>
+                    <CircleUserRound size={32} className={"w-8 h-8 flex-shrink-0"}/>
+                    <div>
                         <h2 className={" text-md "}>{order.user.userFirstName + " " + order.user.userLastName}</h2>
-                        <h2 className={" text-md text-gray-500"}>{order.user.userEmail}</h2>
+                        <h2 className={" text-md text-gray-500 break-all"}>{order.user.userEmail}</h2>
                     </div>
                 </div>
                 <h2 className={" text-lg font-medium"}>${calculateOrderPrice(order)}</h2>
